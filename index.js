7fd8e221f995b3ae50a0a0af5e20b695e97a1413
Candice Thomason <candice.thomason@gmail.com>
2024-03-02 12:26:01 +1100

    add a few tests

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code in this file!
+const currentUser = 'Candice Thomason';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser+'!';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser+'!';
